ed266dac01a1f715d302473ccf57d16c8df9913d
Konstantin Kaploon <kostia@onetwotrade.com>
2015-10-16 16:05:41 +0300

    added Flux (McFly) and implemented reply submition

--- comment.js
+++ comment.js
@@ -1,3 +1,61 @@
+function guidGenerator() {
+  var S4 = function() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+  };
+  return (S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4());
+}
+
+var Flux = new McFly();
+
+function submitReply(reply){
+  //will be simplified once a backend is implemented
+  _data.forEach(function (comment) {
+    pushReplay(reply, comment);
+  });
+}
+
+function pushReplay(reply, comment){
+  if(comment.key === reply.commentId){
+    comment.nested.push({
+      name: reply.user,
+      points: reply.points,
+      text: reply.text,
+      time: new Date().getTime(),
+      key: guidGenerator()
+    });
+  } else if(comment.nested) {
+    comment.nested.forEach(function (nestComment) {
+      pushReplay(reply, nestComment);
+    });
+  }
+}
+
+function getState() {
+  return {
+    comments: CommentStore.getComments()
+  }
+}
+
+var CommentStore = Flux.createStore({
+  getComments: function () {
+    return _data;
+  }
+}, function (payload) {
+  if(payload.actionType === "SUBMIT_REPLY"){
+    submitReply(payload.reply);
+    CommentStore.emitChange();
+  }
+});
+
+var CommentActions = Flux.createActions({
+  submitReply: function (reply) {
+    return {
+      actionType: "SUBMIT_REPLY",
+      reply: reply
+    }
+  }
+});
+
 var ReplyBox = React.createClass({
   getInitialState: function () {
     return {
@@ -16,7 +74,13 @@ var ReplyBox = React.createClass({
     this.setState({text: event.target.value});
   },
   submit: function () {
-
+    CommentActions.submitReply({
+      user: this.state.user,
+      points: this.state.points,
+      text: this.state.text,
+      commentId: this.props.commentId
+    });
+    this.props.toggle();
   },
   render: function () {
     var containerClass = "well col-sm-12";
@@ -57,7 +121,7 @@ var CommentBox = React.createClass({
     if(this.props.comment.nested){
       nestedComments = this.props.comment.nested.map(function (nested) {
         return (
-          <div className="nested comment">
+          <div className="nested comment" key={nested.key}>
             <CommentBox comment={nested}/>
           </div>
         );
@@ -73,10 +137,10 @@ var CommentBox = React.createClass({
         <div className="comment-body">
           <a href="#">{this.props.comment.name}</a>
           <span className="small points">{this.props.comment.points} Points</span>
-          <span>{moment(this.props.comment.time).fromNow()}</span>
+          <span>{moment(this.props.comment.time).fromNow()} </span>
           <div>{this.props.comment.text}</div>
           <a onClick={this.toggleReply}>reply</a>
-          <ReplyBox hidden={!this.state.reply}/>
+          <ReplyBox toggle={this.toggleReply} hidden={!this.state.reply} commentId={this.props.comment.key}/>
           {nestedComments}
         </div>
       </div>
@@ -85,12 +149,20 @@ var CommentBox = React.createClass({
 });
 
 var CommentBoxContainer = React.createClass({
+  mixins: [CommentStore.mixin],
+  getInitialState: function () {
+    return getState();
+  },
+  onChange: function () {
+    this.setState(getState());
+    console.log(this.state);
+  },
   render: function () {
     return (
       <div>
-        {this.props.data.map(function (comment) {
+        {this.state.comments.map(function (comment) {
           return (
-            <div className="col-sm-12 comment">
+            <div className="col-sm-12 comment" key={comment.key}>
               <CommentBox comment={comment} />
             </div>
           )
@@ -100,53 +172,62 @@ var CommentBoxContainer = React.createClass({
   }
 });
 
-var data = [{
+
+var _data = [{
   name: "someNameA",
   points: 156,
   text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque effic scelerisque effic sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu",
   time: new Date().getTime()-1000*60*60*4,
+  key: guidGenerator(),
   nested: [{
     name: "someNameC",
     points: 25,
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu",
     time: new Date().getTime()-1000*60*60*3,
+    key: guidGenerator(),
     nested: [{
       name: "someNameD",
       points: 205,
       text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque effic scelerisque effic sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque effic scelerisque effic sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu",
-      time: new Date().getTime()-1000*60*60*2
+      time: new Date().getTime()-1000*60*60*2,
+      key: guidGenerator()
     }]
   },{
     name: "someNameD",
     points: 15,
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu",
-    time: new Date().getTime()-1000*60*60*1
+    time: new Date().getTime()-1000*60*60*1,
+    key: guidGenerator()
   }]
 }, {
   name: "someNameC",
   points: 25,
   text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu",
   time: new Date().getTime()-1000*60*60*3,
+  key: guidGenerator(),
   nested: [{
     name: "someNameD",
     points: 205,
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu",
-    time: new Date().getTime()-1000*60*60*2
+    time: new Date().getTime()-1000*60*60*2,
+    key: guidGenerator()
   }]
 }, {
   name: "someNameC",
   points: 25,
   text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu",
   time: new Date().getTime()-1000*60*60*3,
+  key: guidGenerator(),
   nested: [{
     name: "someNameD",
     points: 205,
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu",
-    time: new Date().getTime()-1000*60*60*2
+    time: new Date().getTime()-1000*60*60*2,
+    key: guidGenerator()
   }]
 }];
 
 ReactDOM.render(
-  <CommentBoxContainer data={data} />,
+  <CommentBoxContainer />,
   document.getElementById("comments")
 );
