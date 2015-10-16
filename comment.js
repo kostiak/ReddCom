c764bde1e81d1780cde956dd6418a3d3c27e0f19
Konstantin Kaploon <kostia@onetwotrade.com>
2015-10-16 12:41:28 +0300

    added ReplyBox

--- comment.js
+++ comment.js
@@ -1,4 +1,57 @@
+var ReplyBox = React.createClass({
+  getInitialState: function () {
+    return {
+      user: "",
+      points: 0,
+      text: ""
+    }
+  },
+  userChange: function (event) {
+    this.setState({user: event.target.value});
+  },
+  pointsChange: function (event) {
+    this.setState({points: event.target.value});
+  },
+  textChange: function (event) {
+    this.setState({text: event.target.value});
+  },
+  submit: function () {
+
+  },
+  render: function () {
+    var containerClass = "well col-sm-12";
+    containerClass += this.props.hidden ? " hidden" : "";
+
+    return (
+      <div className={containerClass}>
+        <div className="form-group col-sm-6">
+          <input type="text" placeholder="user" className="form-control" value={this.state.user} onChange={this.userChange}/>
+        </div>
+        <div className="form-group col-sm-6">
+          <input type="number" placeholder="123" className="form-control" value={this.state.points} onChange={this.pointsChange}/>
+        </div>
+        <div className="form-group col-sm-12">
+          <textarea className="form-control" onChange={this.textChange}></textarea>
+        </div>
+        <div className="form-group col-sm-12">
+          <button className="btn btn-default pull-right" onClick={this.submit}>Submit</button>
+        </div>
+      </div>
+    );
+  }
+});
+
 var CommentBox = React.createClass({
+  getInitialState: function () {
+    return {
+      reply: false
+    };
+  },
+  toggleReply: function () {
+    this.setState({
+      reply: !this.state.reply
+    })
+  },
   render: function () {
     var nestedComments = "";
     if(this.props.comment.nested){
@@ -22,7 +75,8 @@ var CommentBox = React.createClass({
           <span className="small points">{this.props.comment.points} Points</span>
           <span>{moment(this.props.comment.time).fromNow()}</span>
           <div>{this.props.comment.text}</div>
-          <a href="#">reply</a>
+          <a onClick={this.toggleReply}>reply</a>
+          <ReplyBox hidden={!this.state.reply}/>
           {nestedComments}
         </div>
       </div>
@@ -49,7 +103,7 @@ var CommentBoxContainer = React.createClass({
 var data = [{
   name: "someNameA",
   points: 156,
-  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu",
+  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque effic scelerisque effic sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu",
   time: new Date().getTime()-1000*60*60*4,
   nested: [{
     name: "someNameC",
@@ -59,7 +113,7 @@ var data = [{
     nested: [{
       name: "someNameD",
       points: 205,
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque effic scelerisque effic sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque dignissim purus sed sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque effic scelerisque effic sit amet, consectetur adipiscing elit. Quisque dignissim purus sed scelerisque efficitu",
       time: new Date().getTime()-1000*60*60*2
     }]
   },{
